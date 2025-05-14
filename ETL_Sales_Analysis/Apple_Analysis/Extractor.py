902434a1cdcd8e39a4b5420b98b6960ed28a533b
Mahmoud Mohamed Ahmed <113009688+Mhmod-Mhmd@users.noreply.github.com>
2025-05-14 16:09:12 +0300

    Update Extractor.py file

--- ETL_Sales_Analysis/Apple_Analysis/Extractor.py
+++ ETL_Sales_Analysis/Apple_Analysis/Extractor.py
@@ -1,11 +1,32 @@
 # Databricks notebook source
-class Extractor:
+# import Reader_Factory
+# %run "./Reader_Factory"
 
-    def __init__(self):
-        pass
-
+class DataExtractor:
+    """
+    Implement steps for extract or read the data 
+    """
     def extract(self):
-        pass
+        customerInputDF = (
+                            GetData().
+                            get_data_source(
+                                            data_type = 'csv', 
+                                            path = "/FileStore/tables/Apple_Data/Customer_Updated.csv")
+                            .get_data_frame()
+                            )
+        
+        transactionInputDF =(
+                            GetData.
+                            get_data_source(
+                                            data_type= 'csv',
+                                            path= "/FileStore/tables/Apple_Data/Transaction_Updated.csv")
+                            .get_data_frame() 
+                            )
+        
+        InputDFs = {'customerInputDF': customerInputDF,
+                    'transactionInputDF': transactionInputDF }
+        
+        return InputDFs
 
 # COMMAND ----------
 
