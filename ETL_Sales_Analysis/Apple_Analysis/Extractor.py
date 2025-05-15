4fab8bbede2f58c6237cb41e21ea336915bba227
Mahmoud Mohamed Ahmed <113009688+Mhmod-Mhmd@users.noreply.github.com>
2025-05-15 12:44:39 +0300

    Update Extractor.py

--- ETL_Sales_Analysis/Apple_Analysis/Extractor.py
+++ ETL_Sales_Analysis/Apple_Analysis/Extractor.py
@@ -1,32 +1,49 @@
 # Databricks notebook source
-# import Reader_Factory
+
 # %run "./Reader_Factory"
+import Reader_Factory
 
 class DataExtractor:
     """
     Implement steps for extract or read the data 
     """
     def extract(self):
-        customerInputDF = (
+        try:
+            customer_DF = (
                             GetData().
                             get_data_source(
                                             data_type = 'csv', 
                                             path = "/FileStore/tables/Apple_Data/Customer_Updated.csv")
                             .get_data_frame()
-                            )
+                                )
+            
+            transaction_DF = (
+                                GetData.
+                                get_data_source(
+                                                data_type= 'csv',
+                                                path= "/FileStore/tables/Apple_Data/Transaction_Updated.csv")
+                                .get_data_frame() 
+                                )
+            
+            products_DF = (
+                            GetData
+                            .get_data_source('csv', "/FileStore/tables/Apple_Data/Products_Updated.csv")
+                            .get_data_frame() )
+            
+            InputDFs = {'customerDF': customer_DF,
+                        'transactionDF': transaction_DF,
+                        'productDF': products_DF}
+            
+        except Exception as e:
+            print(f"An Error occured while Extracting data in extract() method : {e}")
+
+        else:
+            return InputDFs
         
-        transactionInputDF =(
-                            GetData.
-                            get_data_source(
-                                            data_type= 'csv',
-                                            path= "/FileStore/tables/Apple_Data/Transaction_Updated.csv")
-                            .get_data_frame() 
-                            )
-        
-        InputDFs = {'customerInputDF': customerInputDF,
-                    'transactionInputDF': transactionInputDF }
-        
-        return InputDFs
+
+# s = DataExtractor()
+# dfs = s.extract()
+# dfs['productDF'].show()
 
 # COMMAND ----------
 
